9002e9439ec25cb33ef20a69e2a5639b1179868c
MArcSiddiqui <MArcSiddiqui@gmail.com>
2026-01-12 22:18:57 +0500

    rename query pushed

--- Presentation/Nop.Web/wwwroot/MiscellaneousFile/399218be-fc4d-4ff8-8d81-c23af0dd410g-TableRenameQuery.cs
+++ Presentation/Nop.Web/wwwroot/MiscellaneousFile/399218be-fc4d-4ff8-8d81-c23af0dd410g-TableRenameQuery.cs
@@ -1,4 +1,9 @@
-IF OBJECT_ID('FeePayments') IS NOT NULL
+IF OBJECT_ID('FeePayments') IS NOT NULL AND OBJECT_ID('FeePayment') IS NOT NULL
 BEGIN
 EXEC SP_RENAME 'FeePayments', 'FeePayment'
+END
+
+IF OBJECT_ID('AcadamicYearTerm') IS NOT NULL AND OBJECT_ID('AcademicYearTerm') IS NOT NULL
+BEGIN
+EXEC SP_RENAME 'AcadamicYearTerm', 'AcademicYearTerm'
 END
