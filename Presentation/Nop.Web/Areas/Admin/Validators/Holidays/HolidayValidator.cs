bc42aaf16a25001b9a7893e4ab881c047c51b9aa
muhammad-ahmer-iqbal <ahmeriqbal17@gmail.com>
2026-02-05 21:27:44 +0500

    in progress work

---
+++ Presentation/Nop.Web/Areas/Admin/Validators/Holidays/HolidayValidator.cs
@@ -0,0 +1,10 @@
+﻿using Nop.Web.Areas.Admin.Models.Discounts;
+using Nop.Web.Areas.Admin.Models.HolidaysNEvents;
+using Nop.Web.Framework.Validators;
+
+namespace Nop.Web.Areas.Admin.Validators.Holidays
+{
+    public class HolidayValidator : BaseNopValidator<HolidayModel>
+    {
+    }
+}
